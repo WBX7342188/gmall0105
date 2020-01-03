e63006ddeb8b26fcdc6403cde7a6c54c43a8334c
wangbaoxin <wbx2188@126.com>
2020-01-03 10:37:21 +0800

    20200103

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/service/UserService.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.user.service;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface UserService {
+    List<UmsMember> getAllUser();
+
+    List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId);
+}
